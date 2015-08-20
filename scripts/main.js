b72851365ae07021051fde54a25e3040c6900042
JamesBreeden <breedenjamesw@gmail.com>
2015-08-20 11:16:54 -0400

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,36 @@
-      $(window).on("scroll", function() {
-            var headerHeight = document.getElementById('header').clientHeight;
-            var fromTop = $(window).scrollTop();
-            $("#navSection").toggleClass("sticky", (fromTop > headerHeight));
-        });
+/*-------------- Sticky Navigation ------------------*/
+$(window).on("scroll", function() {
+      var headerHeight = document.getElementById('header').clientHeight;
+      var fromTop = $(window).scrollTop();
+      $("#navSection").toggleClass("sticky", (fromTop > headerHeight));
+  });
+/*-------------- End Sticky Navigation ------------------*/
+/*-------------- Accordion ------------------*/
+$(document).ready(function() 
+{
+  function close_accordion_section() 
+    {
+      $('.accordion .accordion-section-title').removeClass('active');
+      $('.accordion .accordion-section-content').slideUp(300).removeClass('open');
+    }
+
+  $('.accordion-section-title').click(function(e) 
+  {
+    // Grab current anchor value
+    var currentAttrValue = $(this).attr('href');
+    if($(e.target).is('.active')) 
+      {
+        close_accordion_section();
+      }
+    else 
+      {
+        close_accordion_section();
+        // Add active class to section title
+        $(this).addClass('active');
+        // Open up the hidden content panel
+        $('.accordion ' + currentAttrValue).slideDown(300).addClass('open'); 
+      }
+    e.preventDefault();
+  });
+});
+/*-------------- End Accordion ------------------*/
